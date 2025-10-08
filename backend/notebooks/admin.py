950fabd5ad2a149afe30cbfacced47501ccc1241
Anastasiia Chumak <chumaana@fit.cvut.cz>
2025-10-08 23:48:37 +0200

    feat: dd Notebook model and admin interface

---
+++ backend/notebooks/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Notebook
+
+
+@admin.register(Notebook)
+class NotebookAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "created_at", "updated_at"]
+    list_filter = ["created_at", "author"]
+    search_fields = ["title", "content"]
